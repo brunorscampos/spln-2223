718fdcc1f2530303a469650b83454aa71231a7d0
Campos <brunitocampos@gmail.com>
2023-03-18 17:49:44 +0000

    TPC4

--- TPC4/tokenizadorspln.py
+++ TPC4/tokenizadorspln.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python3 
 
-# 0. Quebras de pagina
-# 1. Separar pontuação das palavras
+# 0. Quebras de pagina                        OK
+# 1. Separar pontuação das palavras           OK
 # 2. Marcar capitulos                         OK
-# 3. Separar paragrafos de linhas pequenas.
-# 4. Juntar linhas da mesma frase.
-# 5. Uma frase por linha
+# 3. Separar paragrafos de linhas pequenas.   ??
+# 4. Juntar linhas da mesma frase.            OK
+# 5. Uma frase por linha                      OK?
 
-import sys
 import fileinput
 import re
 
@@ -16,12 +15,35 @@ text = ""
 for line in fileinput.input():
     text += line
 
-regex_cap = r".*(CAP[ÍI]TULO \w+).*"
-text = re.sub(regex_cap,"\n# \g<1>",text)
+regex_cap = r".*(CAP[ÍI]TULO \w+).*\r?\n([\wàáéèíìòóúùãõêâîôû0-9\-,\ ]+)\r?\n"
+text = re.sub(regex_cap,"### \g<1> ###\n# \g<2> #",text)
 
-regex_nl = r"([a-z0-9,;\-\–\.\?])\r\n\r\n([\-\–a-z0-9])?"
+regex_nl = r"([a-zàáéèíìòóúùãõêâîôû0-9,;\-\–])\r\n\r\n([\-\–a-z0-9])"
+regex_nl2 = r"\r\n\r\n"
+
+regex_pont = r"([a-zàáéèíìòóúùãõêâîôû0-9)])([!?,;.:][^.])"
+
+regex_sr = r"Sr\."
+regex_sra = r"Sra\."
+regex_prof = r"Prof\."
+regex_profa = r"Profa\."
+
+regex_nl3 = r"([A-Za-zÁÀÉÈàáéèíìòóúùãõêâîôû0-9,;\-\–])\r?\n([\-\–\"\“A-Za-zàáéèíìòóúùãõêâîôû(0-9])"
+
+regex_line = r" (\.”?) ([“A-Z])"
 
 text = re.sub(regex_nl,"\g<1>\n\g<2>",text)
+text = re.sub(regex_nl2,"\n",text)
+
+text = re.sub(regex_sr,"SR",text)
+text = re.sub(regex_sra,"SRA",text)
+text = re.sub(regex_prof,"PROF",text)
+text = re.sub(regex_profa,"PROFA",text)
+
+text = re.sub(regex_pont,"\g<1> \g<2>",text)
+text = re.sub(regex_nl3,"\g<1> \g<2>",text)
+
+text = re.sub(regex_line," \g<1>\n\g<2>",text)
 
 arr_poemas = []
 arr_cartas = []
